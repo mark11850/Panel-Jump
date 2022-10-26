e77978ef85785de25ca9199b7fb9df7423143e6c
mark11850 <Mark.hennessy2@students.ittralee.ie>
2022-10-27 00:38:53 +0100

    finieshed off game at last
    - set print script on final platform
    -checked for simple bugs
    
    Signed-off-by: mark11850 <Mark.hennessy2@students.ittralee.ie>

--- Panel-Jump/Assets/scripts/finish.cs
+++ Panel-Jump/Assets/scripts/finish.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class finish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        print("congrats game passed");
     }
 }
